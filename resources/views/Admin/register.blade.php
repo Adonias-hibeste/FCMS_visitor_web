f021f052827c0c5637fc83fe14473e6ccfbed71e
Adonias-hibeste <adoniashibestegithub@gmail.com>
2024-08-06 01:34:12 -0700

    Authentication

--- resources/views/Admin/register.blade.php
+++ resources/views/Admin/register.blade.php
@@ -16,42 +16,45 @@
                 <main>
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-lg-7">
+                            <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                    <div class="card-header">
+                                        <h3 class="text-center font-weight-light my-4">Create Account</h3>
+                                    </div>
                                     <div class="card-body">
                                         <form action="{{ route('admin.register') }}" method="POST">
-                                   @csrf
-
+                                            @csrf
                                             <div class="row mb-3">
-                                                <div class="col-md-6">
+                                                <div class="col-md-12">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" name="name"id="inputFirstName" type="text" placeholder="Enter your first name" />
-                                                        <label for="inputFirstName"> Name</label>
+                                                        <input class="form-control" name="name" id="inputFirstName" type="text" placeholder="Enter your name" />
+                                                        <label for="inputFirstName">Name</label>
                                                     </div>
                                                 </div>
-                                                
                                             </div>
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" name="email"id="inputEmail" type="email" placeholder="name@example.com" />
+                                                <input class="form-control" name="email" id="inputEmail" type="email" placeholder="name@example.com" />
                                                 <label for="inputEmail">Email address</label>
                                             </div>
                                             <div class="row mb-3">
-                                                <div class="col-md-6">
+                                                <div class="col-md-12">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control"name="password" id="inputPassword" type="password" placeholder="Create a password" />
+                                                        <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Create a password" />
                                                         <label for="inputPassword">Password</label>
                                                     </div>
                                                 </div>
-                                                
                                             </div>
                                             <div class="mt-4 mb-0">
-                                                <div class="d-grid"><button class="btn btn-primary" type="submit">sign up</button></div>
+                                                <div class="d-grid">
+                                                    <button class="btn btn-primary" type="submit">Sign Up</button>
+                                                </div>
                                             </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="login.html">Have an account? Go to login</a></div>
+                                        <div class="small">
+                                            <a href="login.html">Have an account? Go to login</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
